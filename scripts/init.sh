b19ca51f1f7cdde1edec390d656b4755dd2a7d11
thomas-schuster <thomas.schuster@hs-pforzheim.de>
2020-12-21 00:45:24 +0100

    added proper boolean checking to perl modul

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION plperl;"
+psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION bool_plperl;"
 psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION plpython3u;"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/mondial_schema.sql 
