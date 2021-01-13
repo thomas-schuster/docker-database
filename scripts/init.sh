a1310b4960034137baae028b7f7fb35a402b1c88
thomas-schuster <thomas.schuster@hs-pforzheim.de>
2021-01-13 14:02:23 +0100

    changed sh file encoding to unix format

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,4 +5,4 @@ psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION bool_plperl;"
 psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION plpython3u;"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/mondial_schema.sql 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/mondial_data.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d $MONDIALDB -f /sql/mondial_data.sql
--- scripts/readenvironment.sh
+++ scripts/readenvironment.sh
@@ -6,3 +6,4 @@ sed -i "s/MONDIALUSERPASSWORD/${MONDIALUSERPASSWORD}/g" /sql/mondial_schema.sql
 sed -i "s/MONDIALUSER/${MONDIALUSER}/g" /sql/mondial_schema.sql 
 sed -i "s/MONDIAL/${MONDIALDB}/g" /sql/mondial_schema.sql 
 sed -i "s/MONDIAL/${MONDIALDB}/g" /sql/mondial_data.sql 
+
