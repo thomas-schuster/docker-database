f4e92327ad2a4de6dce31a578379ddbb140b926b
boehmphilipp93 <boehmphi@hs-pforzheim.de>
2021-04-28 13:26:18 +0200

    fixed a problem that caused the mondial data to not get insert into the tables

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,10 +5,10 @@ psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION bool_plperl;"
 psql -U "$POSTGRES_USER" -d template1 -c "CREATE EXTENSION plpython3u;"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/mondial_schema.sql 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d $MONDIAL_DB -f /sql/mondial_data.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d mondial -f /sql/mondial_data.sql
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/warehouse_schema.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d warehouse -f /sql/warehouse_data.sql
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/warehouse_schema.sql
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d warehouse -f /sql/warehouse_data.sql
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /sql/bl_schema.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d bundesliga -f /sql/bl_data.sql
