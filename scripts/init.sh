44d12afb79105303148c8b20d613780f7186cb73
Thomas Schuster <thomas.schuster@hs-pforzheim.de>
2025-04-20 23:47:37 +0200

    Grant user rights

--- scripts/init.sh
+++ scripts/init.sh
@@ -15,3 +15,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d mondial -f /sql/mondial_w
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d mondial -f /sql/mondial_warehouse_store_data.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d mondial -f /sql/mondial_warehouse_time_data.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d mondial -f /sql/mondial_warehouse_sales_data.sql
+
+# Grant necessary privileges to the application user
+echo "Granting privileges to ${MONDIALUSER} on database ${MONDIALDB}..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$MONDIALDB" <<-EOSQL
+    GRANT USAGE ON SCHEMA public TO ${MONDIALUSER};
+    GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${MONDIALUSER};
+    GRANT INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ${MONDIALUSER};
+    GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO ${MONDIALUSER};
+EOSQL
+echo "Privileges granted."
